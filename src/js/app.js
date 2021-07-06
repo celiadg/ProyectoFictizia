0eef0e32ef96d50c7c736d4ca586f77087cb7ca7
Aceves Carpintero, Diana <diana.aceves@deiser.com>
2021-07-06 22:12:54 +0200

    Add css scroll-behavior and clean unused js

--- src/js/app.js
+++ src/js/app.js
@@ -1,75 +1,22 @@
-/* MOBILE MENU */
-var menuButton = document.getElementById('js-menu-button'),
-    menu = document.getElementById('js-menu'),
-    body = document.body;
+const menuButton = document.getElementById("js-menu-button");
+const menu = document.getElementById("js-menu");
+const body = document.body;
 
 /* OPEN-CLOSE MOBILE MENU */
-var controlMobileMenu = function () {
-    if (menu.classList.contains('is-visible')) {
-        menu.classList.remove('is-visible');
-        body.classList.remove('no-scroll');
-    } else {
-        menu.classList.add('is-visible');
-        body.classList.add('no-scroll');
-    }
-};
+const controlMobileMenu = () => menu.classList.toggle("is-visible");
 
-/* SMOOTH SCROLLING ANCHOR NAVIGATION */
-var animateScroll = function (anchor, time) {
-    var elem = document.scrollingElement || document.documentElement,
-        style = "scrollTop",
-        unit = "",
-        from = window.scrollY,
-        prop = true,
-        to = document.getElementById(anchor).offsetTop;
-
-    if (!elem) {
-        return;
-    }
-    var start = new Date().getTime(),
-        timer = setInterval(function () {
-            var step = Math.min(1, (new Date().getTime() - start) / time);
-            if (prop) {
-                elem[style] = (from + step * (to - from)) + unit;
-            } else {
-                elem.style[style] = (from + step * (to - from)) + unit;
-            }
-            if (step === 1) {
-                clearInterval(timer);
-            }
-        }, 25);
-    if (prop) {
-        elem[style] = from + unit;
-    } else {
-        elem.style[style] = from + unit;
-    }
-};
-
-/* NAVIGATION MOBILE AND DESKTOP */
+/* NAVIGATION MOBILE */
 const BREAKPOINT_MENU = 640; //Same value of media query change from mobile to desktop menu
-var navigation = function (e) {
-    if (e.target.matches('a')) {
-        //Menu items mobile behavior
-        if (window.innerWidth <= BREAKPOINT_MENU) {
-            controlMobileMenu();
-
-        //Menu items desktop behavior
-        } else {
-            var anchor = e.target.getAttribute('href').substr(1);
-            animateScroll(anchor, 800);
-        }
-    }
+const navigation = (e) => {
+  if (window.innerWidth <= BREAKPOINT_MENU && e.target.matches("a")) {
+    controlMobileMenu();
+  }
 };
+
+/* NAVIGATION MOBILE */
+menu.addEventListener("click", navigation);
 
 /************ EVENTS *************/
 
 /* OPEN-CLOSE MOBILE MENU */
-menuButton.addEventListener('click', controlMobileMenu);
-
-/* NAVIGATION MOBILE AND DESKTOP */
-menu.addEventListener('click', navigation);
-
-//Add some pixels (40px) in the anchor navigation so it does not show on the edge
-window.addEventListener('hashchange', function (e) {
-    window.scrollTo(window.scrollX, window.scrollY - 40);
-});
+menuButton.addEventListener("click", controlMobileMenu);
