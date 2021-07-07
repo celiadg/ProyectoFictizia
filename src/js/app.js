2861ee96ba695a0271253c12fc95ac66b88459b4
Aceves Carpintero, Diana <diana.aceves@deiser.com>
2021-07-07 20:56:14 +0200

    Improve desktop navigation

--- src/js/app.js
+++ src/js/app.js
@@ -5,12 +5,21 @@ const body = document.body;
 /* OPEN-CLOSE MOBILE MENU */
 const controlMobileMenu = () => menu.classList.toggle("is-visible");
 
+/* ADD CLASS ACTIVE TO LINK CLICKED */
+const controlDesktopMenu = (linkclick) => {
+  menu
+    .querySelectorAll("a")
+    .forEach((link) => link.classList.remove("is-active"));
+  linkclick.classList.add("is-active");
+};
+
 /* NAVIGATION MOBILE */
-const BREAKPOINT_MENU = 640; //Same value of media query change from mobile to desktop menu
+const BREAKPOINT_MENU = 768; //Same value of media query change from mobile to desktop menu
 const navigation = (e) => {
-  if (window.innerWidth <= BREAKPOINT_MENU && e.target.matches("a")) {
-    controlMobileMenu();
-  }
+  if (!e.target.matches("a")) return;
+  window.innerWidth <= BREAKPOINT_MENU
+    ? controlMobileMenu()
+    : controlDesktopMenu(e.target);
 };
 
 /* NAVIGATION MOBILE */
